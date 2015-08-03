8b9cb746008f906199252ea69240bbf499665dc5
Luis Lopez <lclg21@utexas.edu>
2015-08-03 10:52:18 -0500

    added documentation

--- ObjExample.java
+++ ObjExample.java
@@ -1,15 +1,18 @@
 // ObjExample.java
 class ObjExample
 {
+   //field named i of type integer (primitive data type)
    int i = 1;
    public static void main (String [] args)
    {
-      // creates first object of type ObjExample
+      // creates first object from the ObjExample class
+      // obj1 is an instance of the class ObjExample
       ObjExample obj1 = new ObjExample ();
       System.out.println ("obj1.i = " + obj1.i);
       obj1.printStmnt ();
       
-      // creates second object of type ObjExample
+      // creates second object from the ObjExample class
+      // obj2 is an instance of the class ObjExample
       ObjExample obj2 = new ObjExample ();
       obj1.i = 2;
       System.out.println ("obj1.i = " + obj1.i);
